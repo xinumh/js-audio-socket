e02ceba3c94b85c2f6fd971fa2cb98629033b2bf
hx <huangxin@wondertek.com.cn>
2021-05-13 20:08:57 +0800

    tijiao

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const fs = require('fs')
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
@@ -12,13 +13,28 @@ app.get('/', (req, res) => {
 app.get('/test.mp3', (req, res) => {
   res.sendFile(__dirname + '/test.mp3')
 });
+app.get('/alawmulaw.js', (req, res) => {
+  res.sendFile(__dirname + '/alawmulaw.js')
+});
 
 io.on('connection', (socket) => {
-  console.log('a user connected');
-  socket.on('voice', (blob) => {
-    console.log('blob', blob)
+  console.log('打开连接');
+  let buf = Buffer.alloc(0)
+  socket.on('voice', (buffer) => {
+    const chunk = new Float32Array(buffer, 0, 4)
+    console.log('buffer',  chunk instanceof Float32Array)
     // socket.broadcast.emit('voice',blob)
-    io.emit('voice', blob)
+    const int16Chunk = new Uint8Array(new Int16Array(chunk))
+    buf = Buffer.concat([buf, buffer], buf.length + buffer.length)
+    io.emit('voice', chunk)
+  });
+  socket.on('disconnect', () => {
+    console.log('断开连接');
+    console.log('buf', buf instanceof Float32Array)
+    fs.writeFile('teee12.pcm', buf, (err)=>{
+      console.log('err', err)
+      console.log('文件已被保存')
+    })
   });
 });
 
